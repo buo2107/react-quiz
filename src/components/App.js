c67d7f88f66420352b412429fed96c7302ae9cc6
buo2107 <vampire91145@gmail.com>
2024-01-04 22:28:37 +0800

    Handling user answer : Show the correct option and caculate points

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ const initialState = {
   // Compare this method with how we previous do about isLoading(), isError() methods
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -29,13 +31,26 @@ function reducer(state, action) {
         status: "active",
       };
 
+    case "newAnswer":
+      // Get the current question in order to get the points and correctOption of that it
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error("unknow action");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -58,7 +73,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,21 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          disabled={hasAnswer}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
